51ecbd765efe75b99bd6d3b5acea222fee700ffe
cornel de lange <cfdelange@gmail.com>
2015-11-17 05:46:57 +1300

    added extra new enemy classes

---
+++ Boss.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace fourcolors
+{
+    class Boss : MobileEnemy
+    {
+        public Boss(ObjectGroup.MapObject enemyobject) : base(enemyobject)
+        {
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+                //velocity.Y = -randnumber13;
+            if (image.Position.X < 0)
+            {
+                Dead();
+            }
+            base.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+    }
+}
---
+++ Intruder.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace fourcolors
+{
+    class Intruder : MobileEnemy
+    {
+        public Intruder(ObjectGroup.MapObject enemyobject) : base(enemyobject)
+        {
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            //position smaller than 530, start vibrating
+            if (image.Position.X < 530)
+            {
+                velocity.Y = -3;
+                if (image.Position.X < 0)
+                {
+                    Dead();
+                }
+            }
+            base.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+    }
+}
